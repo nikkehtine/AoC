d780bc43989a4529f23721101a63a7bc268d2c79
nikkehtine <27138416+nikkehtine@users.noreply.github.com>
2025-12-02 22:35:02 +0100

    01 (2025): Proper error handling

--- 2025/01/main.rs
+++ 2025/01/main.rs
@@ -1,21 +1,31 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("Error: only provide the text file");
-        return;
+        eprintln!("Error: expected filename only");
+        return ExitCode::FAILURE;
     }
 
     let input_file = &args[1];
 
-    let file = File::open(input_file).expect("Failed to open file");
+    let file = match File::open(input_file) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to open file: {}", e);
+            return ExitCode::FAILURE;
+        }
+    };
+
     let reader = io::BufReader::new(file);
 
     for line in reader.lines() {
         println!("{}", line.unwrap());
     }
+
+    ExitCode::SUCCESS
 }
